076e9e30764c7a1db4d916471656000aec41af56
IsmielAbir <ismielabir286@gmail.com>
2023-06-27 16:03:20 +0600

    Portfolio

---
+++ Portfolio/base.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <nav class="navbar me-4 ms-4 navbar-expand-lg">
+  <div class="container-fluid">
+    <a id="name1" class="navbar-brand" href="./index.php">Md. Ismiel Hossen Abir</a>
+   
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     
+      
+    </div>
+    <ul class="navbar-nav me-auto mb-2 me mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./projects.php">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="./contact.php">Contact</a>
+        </li>
+
+      </ul>
+  </div>
+</nav>
+    </div>
+    
+    
+
+
+
+<script src="../js/bootstrap.bundle.js"></script>
+    
+</body>
+</html>
---
+++ Portfolio/contact.php
@@ -0,0 +1,60 @@
+<?php include 'base.php'; ?>
+
+<?php
+error_reporting(E_ALL ^ E_WARNING);
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+  
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $dsc = $_POST['dsc'];
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "portfolio";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+  die("Connection Failed");
+}
+
+
+  $sql = "INSERT INTO `contact` (`name`, `email`, `description`) VALUES ('$name', '$email', '$dsc')";
+
+
+mysqli_query($conn, $sql)
+
+
+?>
+
+
+<div class="form-container">
+
+<form action="contact.php" method="post">
+  
+    <h1>Contact Me</h1>
+    
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Full Name</label>
+    <input name="name" type="name" class="form-control" id="name" aria-describedby="emailHelp">
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
+  </div>
+    
+ 
+  <br>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Description</label>
+    <textarea name="dsc" type="email" class="form-control" id="email" rows="10" cols="30" aria-describedby="emailHelp"></textarea>
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <br><br>
+</form>
+</div>
